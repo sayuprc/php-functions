ea98988c1c06b8a3afdc813100cd56b6b8fe945a
sayuprc <41261915+sayuprc@users.noreply.github.com>
2022-07-24 20:35:48 +0900

    test: Rename test function name. (#9)

--- tests/FlattenTest.php
+++ tests/FlattenTest.php
@@ -11,12 +11,12 @@ use PHPUnit\Framework\TestCase;
 class FlatTest extends TestCase
 {
     /**
-     * flat関数のテスト
+     * flatten関数のテスト
      * 引数がある場合、再帰的にフラットにする。
      *
      * @return void
      */
-    public function testFlat(): void
+    public function testFlatten(): void
     {
         $input = [1, 2, 3, 4, 5];
         $expected = [1, 2, 3, 4, 5];
